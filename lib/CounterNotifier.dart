69f9e3559a23423084ae4e5fd792d1f62512556f
joker <fugui@axzo.cn>
2020-03-04 21:58:37 +0800

    增加main3 和 main4 ，对比consumer和selector区别

--- lib/CounterNotifier.dart
+++ lib/CounterNotifier.dart
@@ -2,11 +2,22 @@ import 'package:flutter/foundation.dart';
 
 class CounterNotifier with ChangeNotifier {
   int _count = 0;
+  int _count1 = 0;
+  int _buildCount = 0;
 
   int get count => _count;
 
+  int get count1 => _count1;
+
+  int get buildCount => _buildCount;
+
   increment() {
     _count++;
+    notifyListeners();
+  }
+
+  increment1() {
+    _count1++;
     notifyListeners();
   }
 }
---
+++ lib/main3.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider_demo/CounterNotifier.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Privoder Consumer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProvidePage(title: 'Provider Consumer Home Page'),
+    );
+  }
+}
+
+class ProvidePage extends StatefulWidget {
+  final String title;
+
+  ProvidePage({Key key, this.title}) : super(key: key);
+
+  @override
+  State<ProvidePage> createState() => _ProvidePageState();
+}
+
+class _ProvidePageState extends State<ProvidePage> {
+  CounterNotifier _counter = CounterNotifier();
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ChangeNotifierProvider.value(
+          value: _counter,
+          child: Column(
+            children: <Widget>[
+              Consumer<CounterNotifier>(
+                builder: (context, v, child) {
+                  return Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Increment1 build ${_counter.count1}  count => You have pushed the button this many times:',
+                        ),
+                        Text(
+                          '${_counter.count}',
+                          style: Theme.of(context).textTheme.display1,
+                        ),
+                        FlatButton(
+                            onPressed: () {
+                              _counter.increment();
+                            },
+                            child: Text('Increment'))
+                      ],
+                    ),
+                  );
+                },
+              ),
+              Consumer<CounterNotifier>(
+                builder: (context, v, child) {
+                  return Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Increment build ${_counter.count}  count => You have pushed the button this many times:',
+                        ),
+                        Text(
+                          '${_counter.count1}',
+                          style: Theme.of(context).textTheme.display1,
+                        ),
+                        FlatButton(
+                            onPressed: () {
+                              _counter.increment1();
+                            },
+                            child: Text('Increment1'))
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
